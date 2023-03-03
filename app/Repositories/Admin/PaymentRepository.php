e335ffd68c068d728f23b4fe70a4f6c453596f24
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-03-03 10:06:48 +0700

    Update View on Payment

--- app/Repositories/Admin/PaymentRepository.php
+++ app/Repositories/Admin/PaymentRepository.php
@@ -4,13 +4,18 @@ namespace App\Repositories\Admin;
 
 use App\Interfaces\Admin\PaymentInterface;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Repairment;
 
 class PaymentRepository implements PaymentInterface
 {
     public function index()
     {
+        // Get All Process
+        $process = Repairment::where('status', 'process')->paginate(10);
+
         $data = [
             'user' => Auth::user(),
+            'payments' => $process,
             'title' => 'Payment'
         ];
 
--- resources/views/user/service/payment.blade.php
+++ resources/views/user/service/payment.blade.php
@@ -10,10 +10,39 @@
                 <div class="card-body">
                     
                     
-                    <div class="text-center">
+                    <div class="text-center mb-4">
                         <h1>Payment</h1>
                     </div>
 
+                    @if(empty($payments[0]))
+                        <div class="text-center">No Service Data Yet</div>
+                    @else
+                        {{-- Table --}}
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Vehicle</th>
+                                <th scope="col">Issue</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($payments as $payment)
+                            <tr>
+                                <th scope="row">{{ $payment->id }}</th>
+                                <td>{{ $payment->vehicle_name }}</td>
+                                <td>{{ $payment->issue }}</td>
+                                <td>{{ $payment->status }}</td>
+                                <td>
+                                    <button type="button" class="btn btn-primary text-white btn-update">Generate Payment</button>
+                                </td>
+                            </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    @endif
                 </div>
             </div>
         </div>
